a67b6de813f614e242d2807a9f6738a8679ec8ee
Fermented-coder <kuivalappa@gmail.com>
2024-10-05 15:45:51 +0300

    added some stuff + router fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 z-[100] absolute w-full">
-      <h1 className="text-red-500 text-4xl font-bold cursor-pointer">
-        FLEXNET
+      <h1 className="text-red-600 text-4xl font-bold cursor-pointer border uppercase">
+        Ma<span className="text-3xl mb-6">ul</span>la
       </h1>
       <div>
         <button className="text-white pr-4">Sign In</button>
